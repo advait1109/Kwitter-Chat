873bd3dfc3b78e3bb483b1fd174aaa451801a8f3
advait1109 <86603536+advait1109@users.noreply.github.com>
2022-02-15 16:49:54 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -18,7 +18,16 @@ function getData() { firebase.database().ref("/"+room_name).on('value', function
          firebase_message_id = childKey;
          message_data = childData;
 //Start code
-
+console.log(message_data)
+user_name=message_data['name'];
+message_user=message_data['message'];
+like=message_data['like'];
+name_with_tag="<h4>"+user_name+"<img class='user_tick' src='tick.png'></h4>";
+message_with_tag="<h4 class='message_h4'>"+message_user+"</h4>";
+like_button="<button class='btn btn-warning' id="+firebase_message_id+" value="+like+" onclick='update_like(this.id)'>";
+span_with_tag="<span class='glyphicon glyphicon-thumbs-up'> like: "+like+"</span> </button> <hr>";
+row=name_with_tag+message_with_tag+like_button+span_with_tag;
+document.getElementById("output").innerHTML+=row;
 //End code
       } });  }); }
 getData();
@@ -32,4 +41,12 @@ function send(){
             like:0
       });
       document.getElementById("msg").value=""
+}
+function update_like(message_id){
+      button_id=message_id;
+      likes=document.getElementById(button_id).value;
+      updated_likes=Number(likes)+1;
+      firebase.database().ref(room_name).child(message_id).update({
+            like:updated_likes
+      });
 }
